923936caaf94a7579a9ed49a9b69496ace0f8073
calllivecn <c-all@qq.com>
2018-11-20 16:12:14 +0800

    修改：py3.sh 添加依赖，简单自动化^_^

--- py3.sh
+++ py3.sh
@@ -2,10 +2,10 @@
 # date 2018-11-20 14:32:52
 # author calllivecn <c-all@qq.com>
 
+set -e
+
 
 DEPENDS=(zlib1g-dev libbz2-dev libssl-dev libncurses5-dev libsqlite3-dev libreadline-dev tk-dev libgdbm-dev libdb-dev libpcap-dev xz-utils libexpat1-dev liblzma-dev libffi-dev libc6-dev)
-
-#sudo apt install "$DEPENDS"
 
 install_depends(){
 for i in $(seq 0 $[${#DEPENDS[@]} - 1]);
@@ -14,5 +14,12 @@ do
 done
 }
 
+install_depends
 
+./configure --prefix=/opt/calllivecn/py371 --with-lto --enable-optimizations
 
+CPUs=$(python3 -c 'import os;print(os.cpu_count())')
+
+time { make -j $CPUs; }
+
+time { sudo make install; }
