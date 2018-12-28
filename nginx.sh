11e0eec3b4b066cffe005c1efd492e67fbf35882
calllivecn <c-all@qq.com>
2018-12-28 10:32:44 +0800

    新文件：libdepend.sh 新文件：nginx.sh 修改：  py3.sh

---
+++ nginx.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# date 2018-12-28 10:07:37
+# author calllivecn <c-all@qq.com>
+# https://github.com/calllivecn
+
+
+DEPENDS=(zli1g-dev libssl-dev libpcre3)
+
+. "$(dirname ${0})"/libdepends.sh
+
+nginx__version__test="1.15.2"
+
+PREFIX="/usr/local/nginx"
+
+#./configure --prefix="${PREFIX}" \
+#			--with-http_ssl_module \
+#			--with-http_realip_module \
+#			--with-http_gzip_static_module
+
+./configure --prefix="${PREFIX}" \
+			--user=nginx --group=nginx \
+			--with-http_ssl_module \
