43924e7c721bdd753bb59c1af8c8fff339bbd6a4
calllivecn <calllivecn@outlook.com>
2018-12-28 10:32:44 +0800

    新文件：libdepend.sh 新文件：nginx.sh 修改：  py3.sh

---
+++ libdepend.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# date 2018-11-20 14:32:52
+# author calllivecn <c-all@qq.com>
+
+set -e
+
+install_depends(){
+for i in $(seq 0 $[${#DEPENDS[@]} - 1]);
+do
+	sudo apt install ${DEPENDS[$i]}
+done
+}
+
+install_depends
+
--- py3.sh
+++ py3.sh
@@ -7,14 +7,8 @@ set -e
 
 DEPENDS=(zlib1g-dev libbz2-dev libssl-dev libncurses5-dev libsqlite3-dev libreadline-dev tk-dev libgdbm-dev libdb-dev libpcap-dev xz-utils libexpat1-dev liblzma-dev libffi-dev libc6-dev)
 
-install_depends(){
-for i in $(seq 0 $[${#DEPENDS[@]} - 1]);
-do
-	sudo apt install ${DEPENDS[$i]}
-done
-}
+. "$(dirname ${0})"/libdepends.sh
 
-install_depends
 
 ./configure --prefix=/opt/calllivecn/py371 --with-lto --enable-optimizations
 
