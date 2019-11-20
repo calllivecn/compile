8a47149400f02e219b74b5108cde066dc6f75c7d
calllivecn <calllivecn@outlook.com>
2019-11-20 21:39:13 +0800

    修改：     py3.sh

--- py3.sh
+++ py3.sh
@@ -5,6 +5,7 @@
 set -e
 
 DEPENDS=(zlib1g-dev libbz2-dev libssl-dev libncurses5-dev libsqlite3-dev libreadline-dev libgdbm-dev libdb-dev libpcap-dev xz-utils libexpat1-dev liblzma-dev libffi-dev libc6-dev tk-dev)
+# build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev wget
 
 . "$(dirname ${0})"/libdepends.sh
 
@@ -15,4 +16,6 @@ CPUs=$(python3 -c 'import os;print(os.cpu_count())')
 
 time { make -j $CPUs; }
 
-time { sudo make install; }
+time { sudo make altinstall; python3.8 --version}
+
+#time { sudo make install; }
